228e226be8b16df52db22cfaf4a8154a469d55c3
Patrick Armstrong <patricka@uvic.ca>
2011-11-04 14:11:51 -0700

    Add logging, and cli arg parsing

--- testservice.py
+++ testservice.py
@@ -4,8 +4,10 @@ class TestService(Service):
 
     def __init__(self, *args, **kwargs):
         super(TestService, self).__init__(*args, **kwargs)
-        print "My config is: %s" % self.CFG
+        self.log = self.get_logger()
 
+        self.log.info("%s started" % self.__class__.__name__)
+        self.log.info("config: %s" % self.CFG)
 
 if __name__ == "__main__":
     TestService()
